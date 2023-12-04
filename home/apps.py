4b837d54e01db5cfc78f14688e2327ab03806b76
Brack Harmon <brack02harmon@gmail.com>
2023-12-04 13:33:25 -0600

    google translate removed

--- home/apps.py
+++ home/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
-
-class TranslateConfig(AppConfig):
-    name = 'google_translate' 
